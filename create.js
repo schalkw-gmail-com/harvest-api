c0129b997ce96777fecae0c4577bcf8f3a5c9580
schalkw-gmail-com <schalkw@gmail.com>
2019-04-17 00:11:55 +0200

    start
    basic start with serverless to get the api working on aws

---
+++ create.js
@@ -0,0 +1,24 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "harvest",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      id: uuid.v1(),
+      project: data.content,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
